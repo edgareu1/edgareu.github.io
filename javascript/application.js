74a083e15f555675caa075667609424f1bebe2bb
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-03 18:48:02 +0000

    Improve the JS typeBannerCode function dependency uppon the HTML file

--- javascript/application.js
+++ javascript/application.js
@@ -1,5 +1,4 @@
 // Prepare the app JS here
 import { typeBannerCode } from './components/type-banner-code.js'
 
-let codeContainer = document.querySelector('.banner-code-container');
-typeBannerCode(codeContainer);
+typeBannerCode();
