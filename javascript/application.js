c483448b684bedb6575c0c644d1793545ff344ee
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-03 18:41:03 +0000

    Export the JS typeBannerCode function into the HTML file

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,5 @@
 // Prepare the app JS here
+import { typeBannerCode } from './components/type-banner-code.js'
+
+let codeContainer = document.querySelector('.banner-code-container');
+typeBannerCode(codeContainer);
--- javascript/components/type-banner-code.js
+++ javascript/components/type-banner-code.js
@@ -43,3 +43,5 @@ function typeBannerCode(codeContainer) {
 
   typeCode();
 }
+
+export { typeBannerCode };
