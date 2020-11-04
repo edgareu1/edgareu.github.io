228b4f6a0f80ff46e21a585475502da47cde3379
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-04 22:24:16 +0000

    Solve the git error: object file is empty

--- javascript/application.js
+++
@@ -1,6 +0,0 @@
-// Prepare the app JS here
-import { smoothNavbarAnchors } from './components/smooth-navbar-anchors.js'
-import { typeBannerCode } from './components/type-banner-code.js'
-
-smoothNavbarAnchors();
-typeBannerCode();
