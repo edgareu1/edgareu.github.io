94a0cd8ec6b36354518d10b91a130a1f816b4024
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-05 23:29:26 +0000

    Create the JS animatePillBar function

---
+++ javascript/components/animate-pill-bar.js
@@ -0,0 +1,22 @@
+function animatePillBar() {
+  const anchors = document.querySelectorAll(".pill-items .pill-item");
+  const tabItems = document.querySelectorAll(".tab-items .tab-item");
+
+  for (let anchor of anchors) {
+    const anchorRef = anchor.getAttribute('data-target');
+
+    anchor.addEventListener('click', () => {
+      if (!anchor.classList.contains('active')) {
+        for (let i = 0; i < anchors.length; i++) {
+          anchors[i].classList.remove('active');
+        }
+
+        anchor.classList.add('active');
+
+        for (let i = 0; i < tabItems.length; i++) {
+          (tabItems[i].id == anchorRef) ? tabItems[i].classList.add('active') : tabItems[i].classList.remove('active');
+        }
+      }
+    });
+  }
+}
