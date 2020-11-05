97a1390d300277518dccd21665fda6c7781f4404
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-05 17:34:18 +0000

    Improve the variable names in the JS techBackAnimation function

--- javascript/components/tech-back-animation.js
+++ javascript/components/tech-back-animation.js
@@ -1,8 +1,8 @@
 import { updateCanvasDimensions } from './update-canvas-dimensions.js'
 
 function techBackAnimation(canvasElement) {
-	var words = { "Ruby": 24, "Rails": 20, "JavaScript": 18, "HTML": 16, "CSS": 15, "SQL": 8, "REGEX": 7, "Canvas": 4, "Git": 11, "GitHub": 10, "Bootstrap": 8, "AJAX": 13, "jQuery": 9, "Web API": 11, "Stripe": 7, "Heroku": 5 };
-  var wordsAttr = [];
+	var techList = { "Ruby": 24, "Rails": 20, "JavaScript": 18, "HTML": 16, "CSS": 15, "SQL": 8, "REGEX": 7, "Canvas": 4, "Git": 11, "GitHub": 10, "Bootstrap": 8, "AJAX": 13, "jQuery": 9, "Web API": 11, "Stripe": 7, "Heroku": 5 },
+      techAttr = [];
 
   updateCanvasDimensions(canvasElement);
 
@@ -12,31 +12,31 @@ function techBackAnimation(canvasElement) {
         canvasHeight = canvasElement.height;
 
     class Tech {
-      constructor(word) {
-        this.text = word;
+      constructor(key) {
+        this.text = key;
         this.x = Math.random() * canvasWidth;
         this.y = Math.random() * canvasHeight;
-        this.font = words[word] * (1 + (Math.random() * 0.35)) * 3 + 'px arial';
-        this.speed = words[word] / 5;
+        this.font = techList[key] * (1 + (Math.random() * 0.35)) * 3 + 'px arial';
+        this.speed = techList[key] / 5;
       }
     }
 
-		for (let word in words) {
-			wordsAttr.push(new Tech(word));
+		for (let key in techList) {
+			techAttr.push(new Tech(key));
 		}
 
 		function animation() {
-			for (var i = 0; i < wordsAttr.length; i++) {
-				canvas.font = wordsAttr[i].font;
-				canvas.fillText(wordsAttr[i].text, wordsAttr[i].x, wordsAttr[i].y);
-				wordsAttr[i].width = canvas.measureText(wordsAttr[i].text).width;
+			for (var i = 0; i < techAttr.length; i++) {
+				canvas.font = techAttr[i].font;
+				canvas.fillText(techAttr[i].text, techAttr[i].x, techAttr[i].y);
+				techAttr[i].width = canvas.measureText(techAttr[i].text).width;
 				canvas.stroke();
 
-        if (wordsAttr[i].x > canvasElement.width) {
-          wordsAttr[i].x = - wordsAttr[i].width;
-          wordsAttr[i].y = Math.random() * canvasElement.height;
+        if (techAttr[i].x > canvasElement.width) {
+          techAttr[i].x = - techAttr[i].width;
+          techAttr[i].y = Math.random() * canvasElement.height;
         } else {
-          wordsAttr[i].x += wordsAttr[i].speed;
+          techAttr[i].x += techAttr[i].speed;
         }
 			}
 		}
