87a0e228cd7d1b8c7f0be4f340d9d4805ebf2b18
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-05 17:22:59 +0000

    Comment the JS updateCanvasDimensions function

--- javascript/components/update-canvas-dimensions.js
+++ javascript/components/update-canvas-dimensions.js
@@ -1,13 +1,20 @@
+// Function that updates the Canvas dimensions to fill the whole window of the User device
+// Arguments:
+//   canvas: Canvas element that will have its dimensions updated
 function updateCanvasDimensions(canvasElement) {
+  // Get the height and width of the User device
   let windowWidth = window.innerWidth,
       windowHeight = window.innerHeight;
 
+  // Set the canvas dimensions to be equal to the ones of the User device
   canvasElement.width = windowWidth;
   canvasElement.height = windowHeight;
 
+  // Set the canvas container dimensions to equal to the ones of the User device
   canvasElement.style.width = `${windowWidth}px`;
   canvasElement.style.height = `${windowHeight}px`;
 
+  // Make sure the canvas characteristics stay the same
   let canvas = canvasElement.getContext('2d');
   canvas.strokeStyle = 'gray';
   canvas.fillStyle = 'gray';
