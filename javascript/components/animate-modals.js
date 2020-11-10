fad96f95747f348864455e1bd665cde3b540a605
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-10 00:39:43 +0000

    Update the JS animateModals function to take into account multiple modals

--- javascript/components/animate-modals.js
+++ javascript/components/animate-modals.js
@@ -1,20 +1,35 @@
 function animateModals() {
-  const modalButton = document.querySelector('.modal-contributions');
+  const modalButtons = document.querySelectorAll('.btn-1');
   const modalContainer = document.querySelector('.modal-container');
+  const modals = document.querySelectorAll('.modal');
 
-  modalButton.addEventListener("click", () => {
-    document.body.classList.add('modal-active');
-    modalContainer.style.display = "flex";
-    modalContainer.classList.remove('modal-close');
-    modalContainer.classList.add('modal-open');
-  });
+  for (const button of modalButtons) {
+    const dataTarget = button.getAttribute('data-target');
+    const modal = document.getElementById(dataTarget);
+
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      document.body.classList.add('modal-active');
+
+      modal.style.display = "block";
+      modalContainer.style.display = "flex";
+      modalContainer.classList.remove('modal-close');
+      modalContainer.classList.add('modal-open');
+    });
+  }
 
   modalContainer.addEventListener("click", () => {
-    document.body.classList.remove('modal-active');
     modalContainer.classList.add('modal-close');
 
     setTimeout(() => {
+      document.body.classList.remove('modal-active');
+
       modalContainer.style.display = "none";
+
+      for (const modal of modals) {
+        modal.style.display = "none";
+      }
     }, 500);
   });
 }
