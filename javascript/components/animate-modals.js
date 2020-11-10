27709c3caf6c3429d24c3981b77cc3067cd3ec09
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-10 02:09:43 +0000

    Comment the JS animateModals function

--- javascript/components/animate-modals.js
+++ javascript/components/animate-modals.js
@@ -1,32 +1,43 @@
+// Function that animates the app modals opening/closing
 function animateModals() {
+  // Variables:
+  //   modalButtons:   List of the modal buttons
+  //   modalContainer: Container of the modals
+  //   modals:         List of the modals
   const modalButtons = document.querySelectorAll('.btn-modal');
   const modalContainer = document.querySelector('.modal-container');
   const modals = document.querySelectorAll('.modal');
 
+  // For each of the buttons...
   for (const button of modalButtons) {
+    // Get the modal corresponding to the button
     const dataTarget = button.getAttribute('data-target');
     const modal = document.getElementById(dataTarget);
 
+    // If the user clicks on the button...
     button.addEventListener("click", (event) => {
-      event.preventDefault();
+      event.preventDefault();                       // Prevent the anchor default click behavior
+      document.body.classList.add('modal-active');  // Update the body style (overflow: hidden)
+      modal.style.display = "block";                // Display the relevant modal
 
-      document.body.classList.add('modal-active');
-
-      modal.style.display = "block";
+      // Make the modal container visible and add the opening modal animation
       modalContainer.style.display = "flex";
       modalContainer.classList.remove('modal-close');
       modalContainer.classList.add('modal-open');
     });
   }
 
+  // If the user clicks on the modal container (action that is only possible if a modal is open)...
   modalContainer.addEventListener("click", () => {
+    // Add the closing modal animation
     modalContainer.classList.add('modal-close');
 
+    // After the animation finishes...
     setTimeout(() => {
-      document.body.classList.remove('modal-active');
+      document.body.classList.remove('modal-active'); // Turn the body back to normal
+      modalContainer.style.display = "none";          // Hide the modal container
 
-      modalContainer.style.display = "none";
-
+      // Hide the modal
       for (const modal of modals) {
         modal.style.display = "none";
       }
