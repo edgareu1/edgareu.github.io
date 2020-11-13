c74d0a7beead6dda23a6bc11cb1cb2e93e29d55e
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-13 23:01:27 +0000

    Create the JS createGitCalendar function

---
+++ javascript/components/create-git-calendar.js
@@ -0,0 +1,16 @@
+function createGitCalendar() {
+  fetch(`https://api.bloggify.net/gh-calendar/?username=edgareu1`)
+    .then(response => response.text())
+    .then(response => {
+      document.getElementById('modal-contributions').innerHTML = response;
+    })
+    .then(function() {
+      var modalContributions = document.getElementById('modal-contributions');
+      var gitCalendarTitle = modalContributions.querySelector('.js-yearly-contributions h2');
+      var gitCalendarContent = modalContributions.querySelector('.js-calendar-graph svg');
+
+      modalContributions.innerHTML = '';
+      modalContributions.appendChild(gitCalendarTitle);
+      modalContributions.appendChild(gitCalendarContent);
+    });
+}
