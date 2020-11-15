493a27219999bf3ce925c052a597e5bf925d1d45
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-15 22:08:28 +0000

    Solve the asynchronous bug

--- javascript/components/create-git-calendar.js
+++ javascript/components/create-git-calendar.js
@@ -1,6 +1,6 @@
-function createGitCalendar() {
+async function createGitCalendar() {
   const moreInfo = 'For more details please check <a href="https://github.com/edgareu1" target="blank">@edgareu1</a>';
-  GitHubCalendar(".calendar", "edgareu1", { responsive: true, tooltips: true, summary_text: moreInfo });
+  await GitHubCalendar(".calendar", "edgareu1", { responsive: true, tooltips: true, summary_text: moreInfo });
 
   let contribNumbers = document.querySelectorAll('.calendar .contrib-number');
   contribNumbers[2].innerHTML = contribNumbers[1].innerHTML;
