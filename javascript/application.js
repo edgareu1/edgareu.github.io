735823a9fbc8230df539a818842a250d15327c49
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-15 19:57:13 +0000

    Update the git calendar footer message

--- javascript/application.js
+++ javascript/application.js
@@ -15,8 +15,7 @@ animatePillBar();
 smoothNavbarAnchors();
 typeBannerCode();
 
-const moreInfo = 'For more complete info check <a href="https://github.com/edgareu1" style="color: white; text-decoration: none" target="blank">@edgareu1</a>';
-
+const moreInfo = 'For more details please check <a href="https://github.com/edgareu1" target="blank">@edgareu1</a>';
 GitHubCalendar(".calendar", "edgareu1", { responsive: true, tooltips: true, summary_text: moreInfo });
 
 var canvas = document.getElementById('tech-canvas');
