649110cbb690ec196d9465d9241623f3c5ab8b76
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-17 22:00:30 +0000

    Solve the footer animation bug

--- javascript/components/animate-footer.js
+++ javascript/components/animate-footer.js
@@ -4,15 +4,16 @@ function animateFooter() {
 
   // For each of the anchors...
   for (const anchor of footerAnchors) {
-    // If the user hovers over the anchor: add the splash animation
+    // If the user hovers over the anchor: add the 'splash-animation' class
     anchor.addEventListener('mouseover', () => {
-      if (anchor.classList != 'splash') anchor.classList.add('splash');
+      if (!anchor.classList.contains('splash-animation')) {
+        anchor.classList.add('splash-animation');
+      }
     });
 
-    // If the user removes his mouse from the anchor: remove the splash class
-    // Necessary in order for the next animation to work
-    anchor.addEventListener('mouseout', () => {
-      anchor.classList.remove('splash');
+    // When the animation ends: remove the 'splash-animation' class
+    anchor.addEventListener('animationend', () => {
+      anchor.classList.remove('splash-animation');
     });
   }
 }
