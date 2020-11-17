faafbb2178dc5b3d60f43197d827169798424d0b
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-17 19:32:06 +0000

    Improve the way the modal is displayed

--- javascript/components/animate-git-modal.js
+++ javascript/components/animate-git-modal.js
@@ -6,13 +6,11 @@ function animateGitModal() {
   //   modal:          List of the modals
   const modalButton = document.querySelector('.btn-modal');
   const modalContainer = document.querySelector('.modal-container');
-  const modal = document.querySelector('.modal');
 
   // If the user clicks on the button...
   modalButton.addEventListener("click", (event) => {
     event.preventDefault();                       // Prevent the anchor default click behavior
     document.body.classList.add('modal-active');  // Update the body style (overflow: hidden)
-    modal.style.display = "flex";                 // Display the relevant modal
 
     // If the 'modal-contributions' is the one opened, then smoothly scroll to the top of the page
     // Necessary in order for the 'day-tooltip' to work correctly
@@ -38,7 +36,6 @@ function animateGitModal() {
     setTimeout(() => {
       document.body.classList.remove('modal-active'); // Turn the body back to normal
       modalContainer.style.display = "none";          // Hide the modal container
-      modal.style.display = "none";                   // Hide the modal
     }, 350);
   });
 }
