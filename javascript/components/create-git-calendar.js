740c23e78f7b3d67cf513aa153a20ec653aff779
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-30 23:42:19 +0000

    Remove the git-calendar dependency

--- javascript/components/create-git-calendar.js
+++ javascript/components/create-git-calendar.js
@@ -1,10 +1,6 @@
 // Function that creates the git calendar using the 'Bloggify/github-calendar' library
 // Added a small change to personalize it
 async function createGitCalendar() {
-  // Create the git calendar
-  const moreInfo = 'For more details please check <a href="https://github.com/edgareu1" target="blank">#edgareu1</a>';
-  await GitHubCalendar(".calendar", "edgareu1", { responsive: true, tooltips: true, summary_text: moreInfo });
-
   // Make the 'Current streak' equal to the 'Longest streak'
   // Should wait for the git calendar to be created (previous step)
   // As I contribute everyday but usually only pull at the end of the day, it could present misleading information
