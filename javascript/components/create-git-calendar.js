effc7bcd70544d78bcfad77d4850bcf832f898fd
Edgar Marques <edgar.eu1@hotmail.com>
2020-12-01 02:10:51 +0000

    Fetch and display the personalized git-calendar

--- javascript/components/create-git-calendar.js
+++ javascript/components/create-git-calendar.js
@@ -1,44 +1,37 @@
 // Function that creates the git calendar using the 'Bloggify/github-calendar' library
 // Added a small change to personalize it
 async function createGitCalendar() {
-  // Make the 'Current streak' equal to the 'Longest streak'
-  // Should wait for the git calendar to be created (previous step)
-  // As I contribute everyday but usually only pull at the end of the day, it could present misleading information
-  const fullMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  function fetchCalendar() {
+    fetch(`https://api.bloggify.net/gh-calendar/?username=edgareu1`)
+    .then(response => response.text())
+    .then(response => {
+      let calendarHelper = document.createElement("div"),
+          container = document.querySelector('.calendar');
 
-  let listDays = document.querySelectorAll('.calendar rect.day'),
-      dayCount = 0,
-      sum = 0,
-      streak = 0,
-      inStreak = true,
-      dateBeginStreak,
-      dateEndStreak;
+      calendarHelper.innerHTML = response;
+      calendarHelper.querySelector('h2').remove();
 
-  for (let i = listDays.length - 1; i >= 0; i--) {
-    dayCount = parseInt(listDays[i].getAttribute('data-count'));
-    sum += dayCount;
+      let calendar = calendarHelper.querySelector(".js-yearly-contributions");
+      calendar.querySelector(".float-left").innerHTML = 'For more details please check <a href="https://github.com/edgareu1" target="blank">#edgareu1</a>';
 
-    if (!inStreak) { continue; }
+      if (calendar.querySelector("include-fragment")) {
+        setTimeout(fetchCalendar, 500);
 
-    if (dayCount > 0) {
-      if (dateEndStreak  === undefined) { dateEndStreak = new Date(listDays[i].getAttribute("data-date")); }
-      streak++
-    } else if (i < listDays.length - 2) {
-      inStreak = false;
-      dateBeginStreak = new Date(listDays[i + 1].getAttribute("data-date"));
-    }
+      } else {
+        let calendarSVG = calendar.querySelector("svg.js-calendar-graph-svg"),
+            width = calendarSVG.getAttribute("width"),
+            height = calendarSVG.getAttribute("height");
+
+        calendarSVG.removeAttribute("height");
+        calendarSVG.setAttribute("width", "100%");
+        calendarSVG.setAttribute("viewBox", "0 0 " + width + " " + height);
+      }
+
+      container.innerHTML = calendar.innerHTML;
+    });
   }
 
-  const contribNumbers = document.querySelectorAll('.calendar .contrib-number'),
-        textMuted = document.querySelectorAll('.calendar .text-muted'),
-        dateStreak = `${fullMonths[dateBeginStreak.getUTCMonth()]} ${dateBeginStreak.getUTCDate()} - ${fullMonths[dateEndStreak.getUTCMonth()]} ${dateEndStreak.getUTCDate()}`;
-
-  contribNumbers[0].innerHTML = sum + ' total';
-  contribNumbers[1].innerHTML = streak + ' days';
-  contribNumbers[2].innerHTML = streak + ' days';
-
-  textMuted[3].innerHTML = dateStreak;
-  textMuted[5].innerHTML = dateStreak;
+  fetchCalendar();
 }
 
 export { createGitCalendar };
