ec9dde804c439c7a8541a4f6a35dfc2253dba2a1
Edgar Marques <edgar.eu1@hotmail.com>
2021-01-27 22:13:04 +0000

    Add a minimum time for the loading animation to be displayed

--- javascript/components/loading-page-animation.js
+++ javascript/components/loading-page-animation.js
@@ -3,22 +3,36 @@ import { typeBannerCode } from './type-banner-code.js'
 
 function loadingPageAnimation() {
   const loadingScreen = document.getElementById('page-loading'),
+    pageContent = document.getElementById('page-content'),
     canvas = document.getElementById('tech-canvas'),
-    profileContainer = document.getElementById('profile-container');
+    profileContainer = document.getElementById('profile-container'),
+    beginTime = Date.now();
 
   document.addEventListener('readystatechange', () => {
     if (document.readyState === 'complete') {
-      loadingScreen.classList.add('close');
+      const elapsedTime = Date.now() - beginTime;
+      let waitTime = 0;
+
+      if (elapsedTime < 1000) { waitTime = 1000 - elapsedTime; }
 
       setTimeout(() => {
-        document.body.removeChild(loadingScreen);
-        document.body.classList.remove('modal-active');
-
-        typeBannerCode();
-        techWordsAnimation(canvas, profileContainer);
-      }, 350);
+        displayContent(waitTime);
+      }, waitTime);
     }
   });
+
+  function displayContent() {
+    pageContent.style.display = 'block';
+    loadingScreen.classList.add('close');
+
+    setTimeout(() => {
+      document.body.removeChild(loadingScreen);
+      document.body.classList.remove('modal-active');
+
+      typeBannerCode();
+      techWordsAnimation(canvas, profileContainer);
+    }, 350);
+  }
 }
 
 export { loadingPageAnimation };
