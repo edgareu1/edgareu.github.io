af1468a6e677fdb60750d4cbe6d5046ae2a21c8c
Edgar Marques <edgar.eu1@hotmail.com>
2021-01-27 22:24:27 +0000

    Comment the JS loadingPageAnimation function

--- javascript/components/loading-page-animation.js
+++ javascript/components/loading-page-animation.js
@@ -1,6 +1,8 @@
 import { techWordsAnimation } from './tech-words-animation.js'
 import { typeBannerCode } from './type-banner-code.js'
 
+// Function that creates a loading animation while the page loads and, after loading,
+// prepares the page content
 function loadingPageAnimation() {
   const loadingScreen = document.getElementById('page-loading'),
     pageContent = document.getElementById('page-content'),
@@ -8,6 +10,8 @@ function loadingPageAnimation() {
     profileContainer = document.getElementById('profile-container'),
     beginTime = Date.now();
 
+  // When the page finishes loading, display the page content, but make sure the loading
+  // animation lasts at least 1s (1000 ms)
   document.addEventListener('readystatechange', () => {
     if (document.readyState === 'complete') {
       const elapsedTime = Date.now() - beginTime;
@@ -21,10 +25,12 @@ function loadingPageAnimation() {
     }
   });
 
+  // Function that displays the page content
   function displayContent() {
     pageContent.style.display = 'block';
     loadingScreen.classList.add('close');
 
+    // Wait for the completion of the fading animation of the 'close' class
     setTimeout(() => {
       document.body.removeChild(loadingScreen);
       document.body.classList.remove('modal-active');
