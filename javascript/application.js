6d9f6271233872153bc23bb320aee03d465585e0
Edgar Marques <edgar.eu1@hotmail.com>
2021-01-29 02:08:33 +0000

    Refactor, clean and comment the JS backgroundWordsAnimation function

--- javascript/application.js
+++ javascript/application.js
@@ -6,8 +6,6 @@ import { animatePageScroll } from './components/animate-page-scroll.js'
 import { animateTogglePositioning } from './components/animate-toggle-positioning.js'
 import { createGitCalendar } from './components/create-git-calendar.js'
 import { loadingPageAnimation } from './components/loading-page-animation.js'
-import { updateDimensionsCanvas } from './components/update-dimensions-canvas.js'
-import { updateDimensionsNavbar } from './components/update-dimensions-navbar.js'
 
 loadingPageAnimation();
 animateGitModal();
@@ -15,16 +13,4 @@ animateNavbar();
 animatePageScroll();
 animateTogglePositioning();
 animateCarousels();
-
-const canvas = document.getElementById('tech-canvas'),
-  profileContainer = document.getElementById('profile-container'),
-  navbarLinksList = document.querySelector('.nav-links'),
-  navbarMoreLink = document.querySelector('.nav-show-more');
-
-// If the User resizes his device window, update the canvas and navbar dimensions
-window.addEventListener('resize', () => {
-  updateDimensionsCanvas(canvas, profileContainer);
-  updateDimensionsNavbar(navbarLinksList, navbarMoreLink);
-});
-
 createGitCalendar();
