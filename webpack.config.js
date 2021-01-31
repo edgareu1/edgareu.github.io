49ade7ef97c5c3ba7965e69b53678c755057d3bc
Edgar Marques <edgar.eu1@hotmail.com>
2021-01-31 01:09:12 +0000

    Configure webpack.config to generate a new index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/javascript/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "build")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
   module: {
     rules: [
       {
